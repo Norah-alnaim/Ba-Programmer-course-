d406aef7aa8cc1ead7cb4ce67d4ebca678b793bb
Norah Alnaim <norah_alnaim@hotmail.com>
2022-08-26 13:25:14 +0300

    Swift Logical Operators

--- courseL2.playground/Contents.swift
+++ courseL2.playground/Contents.swift
@@ -1,3 +1,21 @@
-import UIKit
+var x = 1
+var y = 4
+var z = x+y
+print(z)
+print(x-z)
+print(y*x)
+print(y/y)
+print(z%y)
+print(x+y*z)
 
-var greeting = "Hello, playground"
+x += 1
+print(x)
+x -= 5
+print(x)
+x *= 9
+print(x)
+x /= 2
+print(x)
+x %= 7
+print(x)
+
